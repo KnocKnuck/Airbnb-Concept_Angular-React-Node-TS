7f587c0579b3f5de3cf4b54da7ebe665ed676c74
Joseph <jrhani@hotmail.com>
2019-08-20 23:39:30 +0300

    Add temps and first rental file, that's it for today ! :D

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './common/header/header.component'
+import { HeaderComponent } from './common/header/header.component';
+import { RentalComponent } from './rental/rental.component';
+import { TempComponent } from './temp/temp.component';
+import { RentalListComponent } from './rental/rental-list/rental-list.component'
+
+const routes: Routes = [
+  {path: '', component: RentalComponent},
+  {path: 'temp', component: TempComponent}
+]
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    RentalComponent,
+    TempComponent,
+    RentalListComponent,
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
